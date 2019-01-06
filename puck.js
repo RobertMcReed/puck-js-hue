e4c6b80fa4fdbe7e3a9d84291701aa0e97cc655f
Robert Reed <robert.mc.reed@gmail.com>
2019-01-06 11:37:30 -0800

    small class tweaks and comments

--- puck.js
+++ puck.js
@@ -3,70 +3,75 @@ const noble = require('noble');
 class Puck {
   constructor(handleClick) {
     // List of allowed devices
-    const { PUCKS } = process.env
-    this.pucks = PUCKS ? PUCKS.split(',') : []
+    const { PUCKS } = process.env;
+    this.pucks = (PUCKS ? PUCKS.split(',') : []);
 
-    // last advertising data received
-    this.lastAdvertising = {}
-    this.handleClick = handleClick
-    this.discoverPucks = this.discoverPucks.bind(this)
-    this.handlePuck = this.handlePuck.bind(this)
+    // last advertising data received for each Puck
+    this.lastAdvertising = {};
+    this.handleClick = handleClick;
+    this.discoverPucks = this.discoverPucks.bind(this);
+    this.handlePuckAdvertising = this.handlePuckAdvertising.bind(this);
   }
 
   discoverPucks(peripheral) {
-    // peripheral.rssi                             - signal strength
-    // peripheral.address                          - MAC address
-    // peripheral.advertisement.localName          - device's name
-    // peripheral.advertisement.manufacturerData   - manufacturer-specific data
-    // peripheral.advertisement.serviceData        - normal advertisement service data
+    // peripheral.rssi                                                  - signal strength
+    // peripheral.address                                               - MAC address
+    // peripheral.advertisement.localName                               - device's name
+    // peripheral.advertisement.manufacturerData                        - manufacturer-specific data
+    // peripheral.advertisement.serviceData                             - normal advertisement service data
+    // peripheral.advertisement.manufacturerData.slice(2).toString()    - user advertised data
+    
     // ignore devices with no manufacturer data
-
     if (!peripheral.advertisement.manufacturerData) return;
 
-    const localName = peripheral.advertisement.localName
+    // only listen for Pucks
+    const { localName } = peripheral.advertisement;
     if (!(localName && localName.includes('Puck.js'))) return;
 
-    // output what we have
-
+    // display info from found puck
     console.log('MAC Address:',
       peripheral.address,
       'Device Name:',
-      localName
+      localName,
+      'Data:',
+      peripheral.advertisement.manufacturerData.slice(2).toString(),
     );
   }
 
-  handlePuck(peripheral) {
-    // do we know this device?
+  handlePuckAdvertising(peripheral) {
+    // are we listening for this Puck?
     if (!this.pucks.includes(peripheral.address)) return;
   
-    // does it have manufacturer data with Espruino/Puck.js's UUID
+    // does it have manufacturer data with Puck's UUID?
     if (!peripheral.advertisement.manufacturerData ||
         peripheral.advertisement.manufacturerData[0]!=0x90 ||
         peripheral.advertisement.manufacturerData[1]!=0x05) return;
   
-    // get just our data
-    const data = peripheral.advertisement.manufacturerData.slice(2)
+    // get the newly advertised message from this Puck
+    const currentAdvertising = peripheral.advertisement.manufacturerData.slice(2).toString();
   
-    // check for changed services
-    const lastAdvertising = this.lastAdvertising[peripheral.address]
+    // get the previously advertised message by this Puck
+    const lastAdvertising = this.lastAdvertising[peripheral.address];
 
-    if (lastAdvertising && lastAdvertising != data.toString()) {
-      this.handleClick(peripheral.address, data);
+    // if the advertisement has changed, pass the data to the handler
+    if (lastAdvertising && lastAdvertising !== currentAdvertising) {
+      this.handleClick(lastAdvertising, currentAdvertising);
     }
   
-    this.lastAdvertising[peripheral.address] = data;
+    // update the last advertisement
+    this.lastAdvertising[peripheral.address] = currentAdvertising;
   }
 
   init() {
-    noble.on('stateChange',  function(state) {
+    noble.on('stateChange',  (state) => {
       if (state != "poweredOn") return;
 
       noble.startScanning([], true);
     });
 
-    noble.on('discover', this.pucks.length ? this.handlePuck : this.discoverPucks);
+    noble.on('discover', this.pucks.length ? this.handlePuckAdvertising : this.discoverPucks);
 
-    noble.on('scanStart', function() { console.log("[INFO] Scanning started."); });
+    noble.on('scanStart', function() { console.log("[INFO] Listening for Puck.js clicks"); });
 
     noble.on('scanStop', function() { console.log("[INFO] Scanning stopped.");});
   }
