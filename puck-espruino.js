a37f0cdd20cb168cbee84756ab378435666c796a
Robert Reed <robert.mc.reed@gmail.com>
2019-01-06 11:37:07 -0800

    removed basic  puck advert

--- puck-espruino.js
+++
@@ -1,8 +0,0 @@
-var presses = 0;
-NRF.setAdvertising({},{manufacturer: 0x0590, manufacturerData:[presses]});
-
-setWatch(function() {
-  presses++;
-  digitalPulse(LED2, 1, 150);
-  NRF.setAdvertising({},{manufacturer: 0x0590, manufacturerData:[presses]});
-}, BTN, {edge:"rising", repeat:1, debounce:20});
