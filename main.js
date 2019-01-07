a0639d006d99e0e0dc4abf8186783568132a0b6f
Robert Reed <robert.mc.reed@gmail.com>
2019-01-06 22:37:20 -0800

    main only runs main now, no discovery

--- main.js
+++ main.js
@@ -6,15 +6,23 @@ const Puck = require('./puck');
 const main = async () => {
   const hue = await initHueProm();
   const printStatus = true;
-  const puck = new Puck(hue.handlePuckClick(printStatus));
+  const puck = new Puck({ handleClick: hue.handlePuckClick(printStatus) });
   puck.init();
 };
 
-const discovery = () => {
-  console.log('[INFO] Searching for Pucks.');
-  const puck = new Puck();
-  puck.init();
-};
+const { PUCKS, USERNAME } = process.env;
+let run = true;
 
-// If Puck address is known, run the main program. Otherwise, discover nearby Pucks.
-process.env.PUCKS ? main().catch(console.log) : discovery();
+if (!PUCKS) {
+  console.log('[ERROR] You must have a comma separated list of PUCKS in a .env file at the root of the project.\n');
+  console.log('[INFO] Run "node discoverPucks.js" with your Puck nearby and powered on to automatically discover and add it to your .env.\n');
+  run = false;
+}
+
+if (!USERNAME) {
+  console.log('[ERROR] You must have a Hue USERNAME in a .env file at the root of the project.\n');
+  console.log('[INFO] Press the link button on your Hue Bridge and then run "node registerDevice.js" to automatically register your device and add it to your .env.\n');
+  run = false;
+}
+
+if (run) main().catch(console.log);
