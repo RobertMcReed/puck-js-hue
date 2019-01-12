b86224332a0a868af965b1ff0df531e684e71d91
Robert Reed <robert.mc.reed@gmail.com>
2019-01-12 15:22:49 -0800

    cleaned up espruino code for linter

--- espruino/puck-advertise-hue.js
+++ espruino/puck-advertise-hue.js
@@ -1,8 +1,3 @@
-const RED = LED1;
-const GREEN = LED2;
-const BLUE = LED3;
-
-// START
 const GROUPS = [
   { name: 'Bedroom', key: 1 },
   { name: 'Living Room', key: 2 },
@@ -10,16 +5,25 @@ const GROUPS = [
   { name: 'Basement', key: 4 },
   { name: 'TV Room', key: 3 },
 ];
-// END
+// DO NOT ADD ANYTHING ABOVE THIS LINE
+
+const RED = LED1; // eslint-disable-line
+const GREEN = LED2; // eslint-disable-line
+const BLUE = LED3; // eslint-disable-line
 
 const state = {
   group: 0,
   toggle: 0,
 };
 
+function info() {
+  const line = arguments.reduce((acc, phrase) => `${acc} ${phrase}`, '[INFO]');
+  console.log(line);
+}
+
 const getNextGroup = () => {
   if (++state.group === GROUPS.length) state.group = 0;
-  
+
   return { groupNum: state.group, key: GROUPS[state.group].key };
 };
 
@@ -31,10 +35,9 @@ const flashGroupNum = (groupNum, color) => {
   let nums = 0;
 
   const flasherHelper = () => {
-    if (nums > groupNum) return;
-    else {
+    if (nums <= groupNum) {
       nums++;
-      digitalPulse(color, 1, 150);
+      digitalPulse(color, 1, 150); // eslint-disable-line
       setTimeout(flasherHelper, 300);
     }
   };
@@ -43,41 +46,41 @@ const flashGroupNum = (groupNum, color) => {
 };
 
 const setAdvertisement = () => {
-  const key = GROUPS[state.group].key;
-  const toggle = state.toggle;
+  const key = GROUPS[state.group].key; // eslint-disable-line
+  const toggle = state.toggle; // eslint-disable-line
   const advert = `${key}-${toggle}`;
-  NRF.setAdvertising(
+  NRF.setAdvertising( // eslint-disable-line
     {},
-    { manufacturer: 0x0590, manufacturerData:[advert]}
+    { manufacturer: 0x0590, manufacturerData: [advert] },
   );
 };
 
 const handleWatch = (e) => {
-  let len = e.time - e.lastTime;
+  const len = e.time - e.lastTime;
 
-  if (len > 0.7) { //longest press, identify group
+  if (len > 0.7) { // longest press, identify group
     const groupNum = state.group;
     flashGroupNum(groupNum, RED);
-    console.log(`[INFO] Currently set to ${GROUPS[state.group].name}`);
+    info(`Currently set to ${GROUPS[state.group].name}`);
   } else if (len > 0.3) { // long press, switch groups
     const groupData = getNextGroup();
-    const groupNum = groupData.groupNum;
+    const groupNum = groupData.groupNum; // eslint-disable-line
 
     flashGroupNum(groupNum, BLUE);
-    console.log(`[INFO] Switching to ${GROUPS[state.group].name}`);
+    info(`Switching to ${GROUPS[state.group].name}`);
   } else { // short press, toggle light
-    digitalPulse(GREEN, 1, 250);
+    digitalPulse(GREEN, 1, 250); // eslint-disable-line
     toggleLightGroup();
-    console.log(`[INFO] Toggling ${GROUPS[state.group].name}`);
-    console.log(`[INFO] Toggle: ${state.toggle}`);
+    info(`Toggling ${GROUPS[state.group].name}`);
+    info(`Toggle: ${state.toggle}`);
   }
   setAdvertisement();
 };
 
 const init = () => {
-  console.log(`[INFO] __SET_ADVERT__`);
-  setAdvertisement();
-  setWatch(handleWatch, BTN, { edge: 'falling', repeat: true, debounce: 50 });
+  info('__SET_ADVERT__');
+  setAdvertisement(); // eslint-disable-next-line
+  setWatch(handleWatch, BTN, { edge: 'falling', repeat: true, debounce: 50 }); 
 };
 
 init();
