09e9152070bf80a50b0ecd817fd05e9fb8e19494
Robert Reed <robert.mc.reed@gmail.com>
2019-01-12 15:24:36 -0800

    prep puck fetches light groups and writes to espruino code

--- prepPuck.js
+++ prepPuck.js
@@ -1,5 +1,44 @@
-'use strict';
-
-const fs = require('fs');
+require('dotenv').load();
+const fs = require('fs-extra');
+const { join } = require('path');
+const { log, concat } = require('./lib/util');
 const { initHueProm } = require('./lib/hue');
 
+const cleanGroup = group => ({
+  name: group.name,
+  key: group.id,
+});
+
+const stringifyGroups = (groups) => {
+  const header = 'const GROUPS = [';
+  const makeRow = (acc, { name, key }) => `${acc}\n  { name: '${name}', key: ${key} },`;
+  const groupRows = groups.reduce(makeRow, '');
+
+  return concat(header, groupRows, '\n];\n');
+};
+
+const prepPuck = async () => {
+  if (!process.env.HUE_USERNAME) {
+    log.err('You must run registerDevice.js before prepPuck.js');
+    return;
+  }
+
+  const filename = join(__dirname, 'espruino', 'puck-advertise-hue.js');
+  const demarcation = '// DO NOT ADD ANYTHING ABOVE THIS LINE';
+  const codeRaw = await fs.readFile(filename, 'utf-8');
+
+  const splitCode = codeRaw.split(demarcation);
+  const code = splitCode[1] || splitCode[0];
+  const hue = await initHueProm();
+  log.info('Fetching light groups...');
+  const groups = await hue.getGroups();
+  const cleanedGroups = groups.slice(1).map(cleanGroup);
+  const stringGroups = stringifyGroups(cleanedGroups);
+  const formattedCode = concat(stringGroups, demarcation, code);
+  log.info('Adding GROUPS array to espruino/puck-advertise-hue.js...');
+
+  await fs.writeFile(filename, formattedCode);
+  log.info('Success.');
+};
+
+if (!module.parent) prepPuck();
