05fdc383fded882f1d287c3637e0a06199f63297
Robert Reed <robert.mc.reed@gmail.com>
2019-01-12 15:23:34 -0800

    discover all adverts cleaned

--- discoverAllAdverts.js
+++ discoverAllAdverts.js
@@ -1,4 +1,4 @@
-'use strict';
+const { checkBluetooth, log } = require('./lib/util');
 
 const onDiscovery = (peripheral) => {
   // peripheral.rssi                             - signal strength
@@ -9,24 +9,24 @@ const onDiscovery = (peripheral) => {
   // ignore devices with no manufacturer data
   if (!peripheral.advertisement.manufacturerData) return;
   // output what we have
-  console.log(
+  log.info(
     peripheral.address,
     JSON.stringify(peripheral.advertisement.localName),
-    JSON.stringify(peripheral.advertisement.manufacturerData)
+    JSON.stringify(peripheral.advertisement.manufacturerData),
   );
 };
 
 const discoverAllPucks = () => {
   const noble = require('noble');
 
-  noble.on('stateChange',  function(state) {
-    if (state!="poweredOn") return;
-    console.log("Starting scan...");
+  noble.on('stateChange', (bluetoothState) => {
+    checkBluetooth(bluetoothState);
+
+    log.info('Starting scan...');
     noble.startScanning([], true);
   });
+
   noble.on('discover', onDiscovery);
-  noble.on('scanStart', function() { console.log("Scanning started."); });
-  noble.on('scanStop', function() { console.log("Scanning stopped.");});
 };
 
 if (!module.parent) discoverAllPucks();
