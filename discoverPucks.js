cd6d2aedab453dd94d3db39da7b5c252adb11d8d
Robert Reed <robert.mc.reed@gmail.com>
2019-01-18 19:29:12 -0800

    adding command line flag for timeout

--- discoverPucks.js
+++ discoverPucks.js
@@ -3,12 +3,23 @@ const Puck = require('./lib/puck');
 const updateEnv = require('./lib/updateEnv');
 
 const firstOnly = process.argv.includes('--first');
+const timeout = process.argv.indexOf('--timeout');
 
 const handleNewPuck = async (pucks) => {
   await updateEnv({ key: 'PUCKS', value: pucks, overwrite: true });
-  if (firstOnly) process.exit();
+  if (firstOnly) process.exit(0);
 };
 
-const discoverPucks = () => (new Puck({ handleNewPuck }));
+const discoverPucks = () => {
+  if (timeout > -1) {
+    const sec = process.argv[timeout + 1] || 30;
+
+    setTimeout(() => {
+      process.exit(1);
+    }, sec * 1000);
+  }
+
+  return (new Puck({ handleNewPuck }));
+};
 
 if (!module.parent) discoverPucks();
