af6ef6ca17851ca1d5d9dfe7dc2bee84a37da344
Robert Reed <robert.mc.reed@gmail.com>
2019-01-19 12:35:55 -0800

    fixed bad setup script paths

--- setup.js
+++ setup.js
@@ -3,10 +3,10 @@ const shell = require('shelljs');
 
 const { log: { br, info, err } } = require('./lib/util');
 
-const { PUCKS, HUE_USERNAME } = process.env;
+const { PUCKS, HUE_USERNAME, DEBUG } = process.env;
 
 const exec = (command) => {
-  const res = shell.exec(command, { silent: true });
+  const res = shell.exec(command, { silent: !DEBUG });
   let { code } = res;
 
   if (res.stdout.includes('TypeError')) code = 1;
@@ -44,7 +44,7 @@ const setup = async () => {
     br();
     info('Attempting to register your device with Hue Bridge...');
     handleCode(
-      exec('node registerDevice.js'),
+      exec('node utility/registerDevice.js'),
       'Device registered successfully! HUE_USERNAME written to .env.',
       'Could not register device. Ensure you are on the same network as the Hue Bridge, have pressed the link button within the last 30 seconds, and then try again.',
     );
@@ -57,7 +57,7 @@ const setup = async () => {
   if (!PUCKS) {
     info('Searching for a Puck within range...');
     handleCode(
-      exec('node discoverPucks.js --first --timeout'),
+      exec('node utility/discoverPucks.js --first --timeout'),
       'Puck found and added to .env!',
       'No new Puck found. Make sure your device is powered on, within range, and not connected to any other devices and then try again.',
     );
@@ -69,7 +69,7 @@ const setup = async () => {
   br();
   info('Searching for your Hue Lights and Light Groups...');
   handleCode(
-    exec('node discoverLights.js'),
+    exec('node utility/discoverLights.js'),
     'Lights and Light Groups saved to hue.json.',
     'Something went wrong. Check your network connection, Hue bridge, and then try again.',
   );
@@ -78,7 +78,7 @@ const setup = async () => {
   br();
   info('Preparing Espruino code...');
   handleCode(
-    exec('node prepPuck.js --save'),
+    exec('node utility/prepPuck.js --save'),
     'Default config saved to config.json and Espruino code saved to espruino/puck-advertise-hue.js',
     'Something went wrong. Check your network connection, Hue bridge, and then try again.',
   );
@@ -86,7 +86,7 @@ const setup = async () => {
   // flashPuck
   br();
   info('Attempting to send Espruino code to Puck...');
-  const code = exec('node flashPuck.js');
+  const code = exec('node utility/flashPuck.js');
   const success = !code;
 
   if (success) {
@@ -94,7 +94,7 @@ const setup = async () => {
     info('Success! You are now ready to use Puck to control your Hue lights!');
     br();
     info('By default, all of your light groups have been added to the Puck controller.');
-    info('You can change all of the default settings by editing config.json, followed by running "node prepPuck.js" and then "node flashPuck.js".');
+    info('You can change all of the default settings by editing config.json, followed by running "node utility/prepPuck.js" and then "node utility/flashPuck.js".');
     br();
     info('Take a look at hue.json to see all of the lights connected to your bridge. If you want to control lights individually, simply grab them from hue.json and add them to the lights array in your config.json, and then prep and flash the puck again.');
     br();
